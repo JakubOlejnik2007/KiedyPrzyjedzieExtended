1fc751c162d466932803b27e796b1945ba10fe62
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-05-06 20:34:09 +0200

    Added Roboto fonts to project. Modified bottom navigation. Added logo and header. Created base ui.

--- app/src/main/java/com/example/kiedyprzyjedzieextended/MainActivity.kt
+++ app/src/main/java/com/example/kiedyprzyjedzieextended/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.kiedyprzyjedzieextended
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.example.kiedyprzyjedzieextended.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,5 +27,36 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         navView.setupWithNavController(navController)
+
+
+        for (i in 1 until navView.menu.size()) {
+            navView.menu.getItem(i).setTitle(null)
+        }
+
+        navView.setOnNavigationItemSelectedListener { item: MenuItem ->
+            for (i in 0 until navView.menu.size()) {
+                val itemId = navView.menu.getItem(i).itemId
+                val title = resources.getString(getTitleResById(itemId))
+                navView.menu.getItem(i).setTitle(title)
+            }
+            for (i in 0 until navView.menu.size()) {
+                navView.menu.getItem(i).setTitle(null)
+            }
+
+            item.setTitle(resources.getString(getTitleResById(item.itemId)))
+            onNavDestinationSelected(item, navController)
+        }
+
+    }
+    private fun getTitleResById(itemId: Int): Int {
+
+        val parts = resources.getResourceEntryName(itemId).split("_")
+            .dropLastWhile { it.isEmpty() }
+            .toTypedArray()
+
+        val resourceName = "title_" + parts[1]
+
+        Log.d("nav", resourceName)
+        return resources.getIdentifier(resourceName, "string", packageName)
     }
 }
